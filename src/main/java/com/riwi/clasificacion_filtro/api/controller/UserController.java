2d9f19575d0714b005faba3f7cfdd2e6e52f7be0
David Osorio <daalosx@gmail.com>
2024-06-04 11:35:46 -0500

    Method Put solved

--- src/main/java/com/riwi/clasificacion_filtro/api/controller/UserController.java
+++ src/main/java/com/riwi/clasificacion_filtro/api/controller/UserController.java
@@ -3,14 +3,19 @@ package com.riwi.clasificacion_filtro.api.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.riwi.clasificacion_filtro.api.dto.request.UserReq;
 import com.riwi.clasificacion_filtro.api.dto.response.UserResp;
 import com.riwi.clasificacion_filtro.infrastructure.CrudAbstract.IUser;
 
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import lombok.AllArgsConstructor;
 
 @RequestMapping(path = "/User")
@@ -21,7 +26,15 @@ public class UserController {
   private final IUser userService;
 
   @GetMapping
-  public ResponseEntity<Page<UserResp>> getAll(@RequestParam(defaultValue = "1") int page, @RequestParam(defaultValue = "10") int size) {
+  public ResponseEntity<Page<UserResp>> getAll(@RequestParam(defaultValue = "1") int page,
+      @RequestParam(defaultValue = "10") int size) {
     return ResponseEntity.ok(this.userService.getAll(page, size));
   }
+  
+  @PutMapping(path = "/{id}")
+    public ResponseEntity<UserResp> update(
+            @Validated @RequestBody UserReq request,
+            @PathVariable Long id) {
+        return ResponseEntity.ok(this.userService.update(request, id));
+    }
 }
